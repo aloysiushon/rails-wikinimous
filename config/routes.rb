e08d096b346271cb3708750cc521e591907743c9
Hon Aloysius <aloysiushon@hotmail.com>
2023-08-06 23:35:37 +0800

    Wikinimous: #read - Done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # Read all
+  root to: "articles#index"
+  get "article/:id", to: "articles#show", as: "article"
 end
